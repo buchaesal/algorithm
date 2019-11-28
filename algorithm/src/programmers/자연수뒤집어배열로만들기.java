cd1bc9a83e34dbeda89769eb2119cbe5019ff077
DanbiChoi <eks4116@gmail.com>
2019-11-28 18:28:35 +0900

    일단 집에 간다

---
+++ algorithm/src/programmers/자연수뒤집어배열로만들기.java
@@ -0,0 +1,11 @@
+package programmers;
+
+public class 자연수뒤집어배열로만들기 {
+	public static void main(String[] args) {
+		solution(5);
+	}
+	  public static int[] solution(long n) {
+	      int[] answer = {};
+	      return answer;
+	  }
+}
