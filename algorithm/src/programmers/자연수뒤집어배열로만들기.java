6bdc8cf69c7f9cae0f0f82e5c7e8d667e06b8cce
DanbiChoi <eks4116@gmail.com>
2019-11-28 18:30:37 +0900

    리얼 집간다

--- algorithm/src/programmers/자연수뒤집어배열로만들기.java
+++ algorithm/src/programmers/자연수뒤집어배열로만들기.java
@@ -2,10 +2,11 @@ package programmers;
 
 public class 자연수뒤집어배열로만들기 {
 	public static void main(String[] args) {
-		solution(5);
+		solution(12345);
 	}
 	  public static int[] solution(long n) {
-	      int[] answer = {};
+	      int[] answer = new int[(int)Math.log10(n)+1];
+	      System.out.println((int)Math.log10(n)+1);
 	      return answer;
 	  }
 }
