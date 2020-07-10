c997ec95e9e9cb036e7f6175e652ef40bda8d916
danbi <eks4116@gmail.com>
2020-07-10 16:42:44 +0900

    dp step1

---
+++ algorithm/src/baekjoon/P9095.java
@@ -0,0 +1,19 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ * DP가 익숙해질 때까지 DP만 파기
+ * https://www.acmicpc.net/problem/9095
+ *  1,2,3더하기
+ */
+public class P9095 {
+
+    private static BufferedReader br;
+
+    public static void main(String[] args) {
+        br = new BufferedReader(new InputStreamReader(System.in));
+
+    }
+}
