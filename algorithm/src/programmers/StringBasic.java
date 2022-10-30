acaecb13f834f497f96939ad7432a62f9df3d935
Choi Danbi <eks4116@gmail.com>
2022-10-30 22:05:47 +0900

    다리를 건너는 트럭, 기능개발

--- algorithm/src/programmers/StringBasic.java
+++ algorithm/src/programmers/StringBasic.java
@@ -3,17 +3,27 @@ package programmers;
 public class StringBasic {
 	
 	public static void main(String[] args) {
-		System.out.println(solution("12341"));
+
+//		i = i + 1
+//		i < 5 작아야만 한다. 무조건!!!~!!!
+
+
+		for(int i=0; i < 4; i++){ // for문 반복문~~
+
+			for(int j=0; j<i+1; j++){
+				System.out.print("*");
+			}
+
+			System.out.println();
+		}
+
+
+//		for(int i=0; i < 7; i++){ // for문 반복문~~
+//			System.out.print("*");
+//		}
+
 	}
 	
-		  public static boolean solution(String s) {
-		      
-		      for(int i=0;i<s.length();i++){
-		          if(!(s.charAt(i) >= '0' && s.charAt(i) <= '9')){
-		              return false;
-		          }    
-		      }
-		      return true;
-		  }
+
 		
 }
